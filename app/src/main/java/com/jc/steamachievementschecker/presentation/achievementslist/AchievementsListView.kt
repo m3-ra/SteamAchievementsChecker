4d1a6e3b4ed5e2e96450f48bb40b6052d0b158f2
Jérémy CROS <jeremyc@okeenea.com>
2025-06-23 09:32:17 +0200

    Basic improvement to display: make it readable at least

--- app/src/main/java/com/jc/steamachievementschecker/presentation/achievementslist/AchievementsListView.kt
+++ app/src/main/java/com/jc/steamachievementschecker/presentation/achievementslist/AchievementsListView.kt
@@ -4,15 +4,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jc.steamachievementschecker.core.GameInfo
 import com.jc.steamachievementschecker.presentation.achievementslist.AchievementsListViewModel.AchievementsListUiState
 import com.jc.steamachievementschecker.presentation.achievementslist.AchievementsListViewModel.AchievementsListUiState.Loading
@@ -49,18 +53,27 @@ internal fun GamesList(
 ) {
     PullToRefreshBox(
         onRefresh = onRefresh,
-        isRefreshing = isRefreshing
+        isRefreshing = isRefreshing,
+        modifier = Modifier.systemBarsPadding()
     ) {
         LazyColumn {
             items(games) { game ->
-                Row(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Text(text = "${game.name} - ${game.achievementsPercentage}%")
-                }
+                GameListItem(game)
             }
         }
     }
+}
+
+@Composable
+private fun GameListItem(game: GameInfo) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Text(text = "${game.name} - ${game.achievementsPercentage}%")
+    }
+    HorizontalDivider()
 }
 
 @Preview(showBackground = true)
