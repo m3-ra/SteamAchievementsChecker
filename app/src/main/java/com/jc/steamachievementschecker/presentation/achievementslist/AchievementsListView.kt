170e595529b095539107ed3fca952aed3cc7043e
Jérémy CROS <jeremyc@okeenea.com>
2025-06-27 09:26:31 +0200

    Add top app bar and implement first draft of grid display

--- app/src/main/java/com/jc/steamachievementschecker/presentation/achievementslist/AchievementsListView.kt
+++ app/src/main/java/com/jc/steamachievementschecker/presentation/achievementslist/AchievementsListView.kt
@@ -5,60 +5,126 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jc.steamachievementschecker.core.GameInfo
 import com.jc.steamachievementschecker.presentation.achievementslist.AchievementsListViewModel.AchievementsListUiState
 import com.jc.steamachievementschecker.presentation.achievementslist.AchievementsListViewModel.AchievementsListUiState.Loading
 import com.jc.steamachievementschecker.presentation.achievementslist.AchievementsListViewModel.AchievementsListUiState.Success
+import com.jc.steamachievementschecker.presentation.achievementslist.GameInfoDisplay.GRID
+import com.jc.steamachievementschecker.presentation.achievementslist.GameInfoDisplay.LIST
 import com.jc.steamachievementschecker.presentation.theme.SteamAchievementsCheckerTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun AchievementsListView(
     uiState: AchievementsListUiState,
     onRefresh: () -> Unit
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        when (uiState) {
-            Loading -> Loading()
-            is Success -> GamesList(
-                games = uiState.games,
-                onRefresh = onRefresh,
-                isRefreshing = false
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    var displayType by remember { mutableStateOf(LIST) }
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        "Games achievements %",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            displayType = when (displayType) {
+                                LIST -> GRID
+                                GRID -> LIST
+                            }
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = "Change display layout"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior
             )
+        }
+    ) { innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            when (uiState) {
+                Loading -> Loading()
+                is Success -> GamesList(
+                    games = uiState.games,
+                    displayType = displayType,
+                    onRefresh = onRefresh,
+                    isRefreshing = false
+                )
+            }
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun GamesList(
+private fun GamesList(
     games: List<GameInfo>,
+    displayType: GameInfoDisplay,
     onRefresh: () -> Unit,
     isRefreshing: Boolean
 ) {
     PullToRefreshBox(
         onRefresh = onRefresh,
-        isRefreshing = isRefreshing,
-        modifier = Modifier.systemBarsPadding()
+        isRefreshing = isRefreshing
     ) {
-        LazyColumn {
-            items(games) { game ->
-                GameListItem(game)
+        when (displayType) {
+            LIST -> {
+                LazyColumn {
+                    items(games) { game ->
+                        GameListItem(game)
+                    }
+                }
+            }
+            GRID -> {
+                LazyVerticalGrid(columns = GridCells.Fixed(9)) {
+                    items(games.size) { index ->
+                        GameGridItem(games[index])
+                    }
+                }
             }
         }
     }
@@ -74,6 +140,17 @@ private fun GameListItem(game: GameInfo) {
         Text(text = "${game.name} - ${game.achievementsPercentage}%")
     }
     HorizontalDivider()
+}
+
+@Composable
+private fun GameGridItem(game: GameInfo) {
+    // TODO lots to improve here
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = game.name)
+        Text(text = "${game.achievementsPercentage}%")
+    }
 }
 
 @Preview(showBackground = true)
@@ -103,3 +180,39 @@ private fun AchievementsListViewGamesPreview() {
         )
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+private fun GamesListPreview() {
+    SteamAchievementsCheckerTheme {
+        GamesList(
+            games = listOf(
+                GameInfo(2, "Game abc", 100, "abc"),
+                GameInfo(3, "Game def", 50, "def"),
+                GameInfo(1, "Game xyz", 50, "xyz")
+            ),
+            displayType = LIST,
+            onRefresh = {},
+            isRefreshing = false
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun GamesGridPreview() {
+    SteamAchievementsCheckerTheme {
+        GamesList(
+            games = listOf(
+                GameInfo(2, "Game abc", 100, "abc"),
+                GameInfo(3, "Game def", 50, "def"),
+                GameInfo(1, "Game xyz", 50, "xyz")
+            ),
+            displayType = GRID,
+            onRefresh = {},
+            isRefreshing = false
+        )
+    }
+}
+
+private enum class GameInfoDisplay { LIST, GRID }
